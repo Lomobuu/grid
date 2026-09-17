0f4c2bcaf7c01de6a0e65593a42dba5b42acc053
Simen Fossen <simen.fossen@lyse.net>
2026-09-17 15:46:21 +0200

    fix pathing and remove RGname in core module

--- terraform/environments/prod/core/main.tf
+++ terraform/environments/prod/core/main.tf
@@ -12,9 +12,9 @@ data "azurerm_resource_group" "this" {
 }
 
 module "core" {
-  source = "../../modules/core"
+  source = "../../../modules/core"
 
-  resource_group_name = data.azurerm_resource_group.this.name
   location            = local.location
+  environment         = local.environment
 }
 

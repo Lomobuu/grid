39e6411fb1a1ec18a115b4f11dafcc1abd92e08e
Simen Fossen <simen.fossen@lyse.net>
2026-09-21 10:06:17 +0200

    fix new version of log analytics

--- terraform/environments/prod/core/terraform.tf
+++ terraform/environments/prod/core/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.9.0"
+  required_version = ">= 1.1.0, < 2.0.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 5.2.0"
+      version = ">= 4.35.0, < 6.0.0"
     }
   }
   backend "azurerm" {}
--- terraform/modules/core/main.tf
+++ terraform/modules/core/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "ResourceGroup" {
 
 module "log_analytics" {
   source  = "equinor/log-analytics/azurerm"
-  version = "2.2.0"
+  version = "2.5.0"
 
   workspace_name      = "log-${local.application}-fozzen-${var.environment}"
   resource_group_name = azurerm_resource_group.ResourceGroup.name
